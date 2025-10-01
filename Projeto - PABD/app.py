77577e992a40a9f6b5ec69437c2a9706c1ab8d77
Julia Nascimento <julia.nascimento1@escolar.ifrn.edu.br>
2025-10-01 07:33:30 -0300

    atualização banco

--- Projeto - PABD/app.py
+++ Projeto - PABD/app.py
@@ -1,13 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import mysql.connector
 
 app = Flask(__name__)
 
-def conexaobd():
-    return mysql.connector.connect(
+db = mysql.connector.connect(
         host = "127.0.0.1",
         user = "root",
         senha = "labinfo",
         database = "ajudaibd"
     )
+cursor = db.cursor()
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        telefone = request.form['telefone']
+        nascimento = request.form['nascimento']
+        email = request.form['email']
+        senha = request.form['senha']
+
+        try:
+            cursor.execute(
+                "INSERT INTO usuarios (email, nome, telefone, nascimento, senha) VALUES (%s, %s, %s, %s, %s)",
+                (email, nome, telefone, nascimento, senha)
+            )
+            db.commit()
+            return "Usuário cadastrado com sucesso!"
+        except mysql.connector.IntegrityError:
+            return "Erro: email já cadastrado!"
+
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
